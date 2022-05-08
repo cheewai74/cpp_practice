3f79edc512c99f4bef18054d6278deffff5941b3
moses <academicsnotes@gmail.com>
2022-05-08 19:03:17 +0800

    Commit scripts

---
+++ Basic/do_while_example.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "DO-WHILE:\n";
+    int counter = 1;
+    do{
+        cout << counter << endl;
+        counter++;
+    }while(counter <= 10);
+}
---
+++ Basic/while_loop_example.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "WHILE:\n";
+    int counter = 1;
+    while (counter <= 10){
+        cout << counter << endl;
+        counter++;
+    }
+}
